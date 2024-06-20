5b902b9a8bbe6e3c46fe4c92f846519d5d6fdfeb
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2024-06-20 22:22:42 +0800

    finish 2nd course

--- 00-basics/main.tf
+++ 00-basics/main.tf
@@ -1,28 +1,68 @@
 # immutable file on the local filesystem,
 # it will be recreated every time the configuration is applied
 
+terraform {
+    required_providers { // This block is used to define the required providers for the configuration.
+        local = {
+        source = "hashicorp/local"
+        version = "~> 2.5.1"
+        }
+    }
+    
+}
+
 resource "local_file" "pet" {
-    filename = "./pet.txt"
-    content  = "${var.file_content["pet"]} ${random_pet.pet.id}"
-    file_permission = "0700"
-    depends_on = [ # This is a list of resources that this resource depends on, optionally, before running.
-        random_pet.pet
-    ]
+  filename        = "./pet.txt"
+  content         = "${var.file_content["pet"]} ${random_pet.pet.id}"
+  file_permission = "0700"
+  depends_on = [ # This is a list of resources that this resource depends on, optionally, before running.
+    random_pet.pet
+  ]
+#   lifecycle { // This block is used to configure the behavior of the resource during its lifecycle.
+#     create_before_destroy = true
+#   }
 }
 
 resource "local_file" "cat" {
-    filename = "./cat.txt"
-    content  = "I have a pet cat called ${var.cat_name}"
+    filename        = "./cat.txt"
+    content         = "I have a pet cat called ${var.cat_name}"
+    # content         = data.local_file.dog.content // This is an example of using the content attribute of a data source as the content attribute of a resource.
     file_permission = "0700"
+    count = 3
+#   lifecycle { // This block is used to configure the behavior of the resource during its lifecycle.
+#     prevent_destroy = true
+#   }
 }
 
 resource "random_pet" "pet" {
-    length = var.pet_length
-    prefix = var.pet_prefix[0]
-    separator = var.pet_separator
+  length    = var.pet_length
+  prefix    = var.pet_prefix[0]
+  separator = var.pet_separator
+#   lifecycle {
+#     ignore_changes = [ // This block is used to ignore changes to specific attributes of the resource.
+#       separator
+#     ]
+#   }
 }
 
-output pet-name {
-    value = "${random_pet.pet.id}"
-    description = "The name of the pet"
+# resource "local_file" "fruits" {
+#     filename = var.fruits[count.index]
+#     content  = "This is a ${var.fruits[count.index]}"
+#     count = length(var.fruits)
+# }
+
+resource "local_file" "fruits" {
+    for_each = var.fruits
+    filename = each.value
+    content  = "This is a ${each.value}"
+  
+}
+
+data "local_file" "dog" {
+  filename = "./dog.txt"
+}
+
+output "pet-name" {
+  value       = random_pet.pet.id
+  description = "The name of the pet"
 }
--- 00-basics/variables.tf
+++ 00-basics/variables.tf
@@ -1,33 +1,39 @@
 variable "pet_prefix" {
-    description = "The prefix of the pet name"
-    type        = list(string)
-    default     = ["Tuan", "Puan"]
+  description = "The prefix of the pet name"
+  type        = list(string)
+  default     = ["Tuan", "Puan"]
 }
 
 variable "pet_separator" {
-    description = "The separator between pet name and random string"
-    type        = string
-    default     = " "
+  description = "The separator between pet name and random string"
+  type        = string
+  default     = " "
 }
 
 variable "pet_length" {
-    description = "The length of the random string"
-    type        = number
-    default     = 2
-  
+  description = "The length of the random string"
+  type        = number
+  default     = 2
+
 }
 
 variable "cat_name" {
-    type = string
+  type = string
 }
 
 variable "file_content" {
-    description = "The content of the file"
-    type        = map(string)
-    default     = {
-        pet = "I have a pet"
-        cat = "I have a pet cat called Whiskers"
-    }
+  description = "The content of the file"
+  type        = map(string)
+  default = {
+    pet = "I have a pet"
+    cat = "I have a pet cat called Whiskers"
+  }
+}
+
+variable "fruits" {
+  description = "A list of fruits"
+  type        = set(string)
+  default     = ["apple.txt", "banana.txt", "cherry.txt"]
 }
 
 # Set -> is a collection of distinct elements
